8d543b461fe3b5baf9cf2f8ef3bcc8c8981005bb
Andres Galvan <afgalvan@unicesar.edu.co>
2021-04-27 19:12:13 -0500

    Initial commit

---
+++ src/main/java/co/edu/unicesar/purpose/main/Main.java
@@ -0,0 +1,11 @@
+package co.edu.unicesar.purpose.main;
+
+import lombok.val;
+
+public class Main {
+
+    public static void main(String[] args) {
+        val message = "Hello template!";
+        System.out.println(message);
+    }
+}
